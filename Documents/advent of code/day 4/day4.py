94600f521d5191e3c47a7b96aeec6d94edffe8a3
v4mika <vamikag@gmail.com>
2022-12-05 01:02:31 +0000

    Day 4 part 2

--- Documents/advent of code/day 4/day4.py
+++ Documents/advent of code/day 4/day4.py
@@ -2,6 +2,9 @@ def in_range(begin1, end1, begin2, end2):
     return ((begin1 <= begin2 <= end2 <= end1)
             or (begin2 <= begin1 <= end1 <= end2))
     
+def any_overlap(begin1, end1, begin2, end2):
+    return begin1 <= begin2 <= end1 or begin2 <= begin1 <= end2
+    
 def split_section(section):
     section_lst = section.split('-')
     begin = int(section_lst[0])
@@ -9,15 +12,20 @@ def split_section(section):
     return (begin, end)
 
     
-counter = 0
+complete_overlap_counter = 0
+any_overlap_counter = 0
 with open("input.txt", "r") as input:
     for line in input:
         sections = line.split(',')
         (begin1, end1) = split_section(sections[0])
         (begin2, end2) = split_section(sections[1])
-        if in_range(begin1, end1, begin2, end2):
+        if any_overlap(begin1, end1, begin2, end2):
             print(begin1, end1, begin2, end2)
-            counter += 1
+            any_overlap_counter += 1
+            if in_range(begin1, end1, begin2, end2):
+                complete_overlap_counter += 1
+            
 input.close()
-print(counter)
+print(any_overlap_counter)
+print(complete_overlap_counter)
  
